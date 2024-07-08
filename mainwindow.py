e0e3ec84c8b5c99f0195b15f750f0a3b0d0dce00
Victor Hu <victorhu493@gmail.com>
2024-07-08 18:47:58 +0800

    Cleanse the batch of csv files and render it

--- mainwindow.py
+++ mainwindow.py
@@ -9,7 +9,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QVBoxLayo
 from PyQt5.QtCore import QUrl
 import os
 import folium
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+#from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
@@ -30,7 +30,25 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
         self.csvTableView = QTableView()
         self.csvTableLayout = QVBoxLayout(self.tableWidget)  # Add to Widget1's layout
         self.csvTableLayout.addWidget(self.csvTableView)
+    def displayCsv(self, df):
+        try:
+            # Use pandas to read the CSV file
+            model = QStandardItemModel(df.shape[0], df.shape[1])
+            model.setHorizontalHeaderLabels(df.columns)
 
+            # Fill data
+            for row in df.itertuples():
+                for col_index, value in enumerate(row[1:]):
+                    item = QStandardItem(str(value))
+                    model.setItem(row.Index, col_index, item)
+
+            # Set up the table view
+            self.csvTableView.setModel(model)
+            self.csvTableView.resizeColumnsToContents()
+            self.csvTableView.setAlternatingRowColors(True)
+            self.csvTableView.horizontalHeader().setStretchLastSection(True)
+        except Exception as e:
+            print("Error reading file:", e)
     def displayCsvInTable(self, filename):
         try:
             # Use pandas to read the CSV file
@@ -69,11 +87,91 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
         self.stackedWidget.setCurrentIndex(4)
 
     def addcsv(self):
-        filename, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "./", "CSV Files (*.csv)")
-        if filename:
-            self.displayCsvInTable(filename)
-
-
+        directory=QFileDialog.getExistingDirectory(self,"Select directory that contains CSV files")
+        #filename, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "./", "CSV Files (*.csv)")
+        if directory:
+            mdf=self.merge_data(directory)
+            cdf=self.cleanse_data(mdf)
+            self.displayCsv(cdf)
+    def cleanse_data(self,merged_df):
+        # 数据偏移纠正
+        merged_df["bmscurrent"] = merged_df["bmscurrent"] * (-1)
+        merged_df["maxcurrent"] = merged_df["maxcurrent"] * (-1)
+        # 数据去重
+        merged_df = merged_df.drop_duplicates()
+        # 按时间和站名排序
+        merged_df['samptime'] = pd.to_datetime(merged_df['samptime'], unit='ms')
+        merged_df['fessamptime'] = pd.to_datetime(merged_df['fessamptime'], unit='ms')
+        merged_df = merged_df.sort_values(by=['station_name', 'stake_name', 'samptime'])
+        merged_df = merged_df.reset_index(drop=True)
+        return merged_df
+    def merge_data(self,folder_path):
+        df_list = []
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".csv"):
+                file_path = os.path.join(folder_path, filename)
+                # 如果你的CSV文件没有表头，你可以手动设置一个
+                header_list = [
+                    "station_code",
+                    "station_name",
+                    "stake_name",
+                    "tag",
+                    "datatype",
+                    "chargingpilenum",
+                    "chargingpileinterfacenum",
+                    "conncetflag",
+                    "charginggunflag",
+                    "electroniclockstatus",
+                    "dcoutputcontactorstatus",
+                    "workstatus",
+                    "outputvoltage",
+                    "outputcurrent",
+                    "demandvoltage",
+                    "demandcurrent",
+                    "chargemode",
+                    "soc",
+                    "batterytype",
+                    "minimumbatterytemperature",
+                    "maximumbatterytemperature",
+                    "cumulativechargetime",
+                    "estimatedfullchargetime",
+                    "maximumbatteryvoltage",
+                    "minimumbatteryvoltage",
+                    "totalactivepower",
+                    "electricityconsumptionamount",
+                    "servicefee",
+                    "chargingtype",
+                    "useridentification",
+                    "tariffmodelnumber",
+                    "servicechargemodelnumber",
+                    "batch",
+                    "status",
+                    "samptime",
+                    "msgnum",
+                    "bmsvoltage",
+                    "bmscurrent",
+                    "guntemperature1",
+                    "guntemperature2",
+                    "guntemperature3",
+                    "guntemperature4",
+                    "maxtemperature",
+                    "maxbmsvoltage",
+                    "maxmonomervoltage",
+                    "maxcurrent",
+                    "ratedtotalvoltage",
+                    "currentvoltage",
+                    "ratedcapacity",
+                    "nominalenergy",
+                    "stateofcharge",
+                    "powertype",
+                    "powerparameter",
+                    "resultsfeedback",
+                    "fessamptime",
+                    "dt"]
+                df = pd.read_csv(file_path, header=None, names=header_list)
+                df_list.append(df)
+        merged_df = pd.concat(df_list, ignore_index=True)
+        return merged_df
     def packageData(self):
         # 读取控件内容并封装成字典
         data = {
